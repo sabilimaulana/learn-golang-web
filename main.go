8d2a15f67451136496666cd940cc749750cd1bb2
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-12 20:31:26 +0700

    Mengambil query string

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -19,6 +21,7 @@ func main() {
 	mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to profile page."))
 	})
+	mux.HandleFunc("/product", productHandler)
 
 	log.Print("Starting web on http://localhost:8080")
 
@@ -42,4 +45,15 @@ func sabiliHandler(w http.ResponseWriter, r *http.Request) {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world! saya sedang belajar Golang."))
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	idNumber, err := strconv.Atoi(id)
+	if err != nil || idNumber < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Product page : %d", idNumber)
 }
