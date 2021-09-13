df379c3412f5e3f626b431ccc33205a7b2f0e928
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-13 20:02:27 +0700

    Template function

--- entity/product.go
+++ entity/product.go
@@ -5,3 +5,14 @@ type Product struct {
 	Name         string
 	Price, Stock int
 }
+
+func (p Product) StockStatus() string {
+	var status string
+
+	if p.Stock < 3 {
+		status = "Stok hampir habis"
+	} else if p.Stock < 10 {
+		status = "Stok terbatas"
+	}
+	return status
+}
